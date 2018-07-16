9e59c6d042dd4545fb87a8c36ffd3452bf4c31ba
dann1 <dann1telecom@gmail.com>
2018-07-16 13:40:20 -0400

    updated copyright

--- src/oca/python/setup.py
+++ src/oca/python/setup.py
@@ -1,5 +1,6 @@
 # Copyright 2018 www.privaz.io Valletech AB
-#
+# Copyright 2002-2018, OpenNebula Project, OpenNebula Systems
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
